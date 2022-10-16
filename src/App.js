3bd2483f9e66ce3968652f9c4e99b7732edcf432
guidomora <guidomorabito161@hotmail.com>
2022-10-16 17:49:45 -0300

    API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Field, Form, Formik } from 'formik';
 import './App.css';
 
@@ -8,17 +9,23 @@ function App() {
         <Formik
         initialValues={{search: ""}}
         onSubmit={async values => {
-          console.log(values)
+          const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+            headers: {
+              "Authorization" : "Client-ID 4VdJyEXw83Pf_coJSI8UbppQgO_sNWnpZAhy5EjAGnU"
+            }
+          })
+          const data = await response.json()
+          console.log(data)
         }}
         >
-          <Form>
-            <h1>Buscar imagen:</h1>
-            <Field type="text" name="search" />
+          <Form className="form">
+            <h1 className="titulo">Buscar imagen:</h1>
+            <Field type="text" name="search" className="buscador"/>
           </Form>
         </Formik>
       </header>
     </div>
   );
 }
-
+ 
 export default App;
